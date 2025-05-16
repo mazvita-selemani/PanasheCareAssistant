3ef5d63d2727684155f9070c196ccec26b53366c
Mazvita Selemani <mazviselemani99@gmail.com>
2025-05-16 12:49:44 +0100

    Add boolean check for login and registration screens to hide bottom navbar

--- app/src/main/java/com/panashecare/assistant/MainActivity.kt
+++ app/src/main/java/com/panashecare/assistant/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.panashecare.assistant
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +9,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -24,22 +19,16 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarDefaults.containerColor
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -88,14 +77,15 @@ class MainActivity : ComponentActivity() {
                 val appColors = AppColors()
 
                 Scaffold(
-                    bottomBar = {
-                        NavigationBar(
-                            containerColor = appColors.primaryDark
-                        ) {
+                    bottomBar =  {
+                        if (currentDestination != "com.panashecare.assistant.Login" && currentDestination != "com.panashecare.assistant.Register") {
+                            NavigationBar(
+                                containerColor = appColors.primaryDark
+                            ) {
 
 
                                 items.forEachIndexed { _, navDestination ->
-                                  val selected = currentDestination == navDestination.routeAsString
+                                    val selected = currentDestination == navDestination.routeAsString
                                     NavigationBarItem(
                                         selected = selected,
                                         onClick = {
@@ -151,7 +141,9 @@ class MainActivity : ComponentActivity() {
                                         )
                                     )
                                 }
+                            }
                         }
+
                     }
                 ) {innerPadding ->
                     AppNavigation(
